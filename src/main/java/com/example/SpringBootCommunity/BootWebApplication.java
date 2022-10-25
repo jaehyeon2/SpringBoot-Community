36342b6c696bcf761549f1bb1c8f42f389ea9b8a
jaehyeon2 <wol5971@gmail.com>
2022-10-25 21:48:51 +0900

    board form&list front(thymeleaf)

--- src/main/java/com/example/SpringBootCommunity/BootWebApplication.java
+++ src/main/java/com/example/SpringBootCommunity/BootWebApplication.java
@@ -15,34 +15,34 @@ import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class BootWebApplication {
-    public static void main(String[] args){
-        SpringApplication.run(BootWebApplication.class, args);
-    }
+	public static void main(String[] args){
+		SpringApplication.run(BootWebApplication.class, args);
+	}
 
-    @Bean
-    public CommandLineRunner runner(UserRepository userRepository,
-                                    BoardRepository boardRepository) throws Exception{
-        return (args)->{
-            User user = userRepository.save(
-                    User.builder()
-                            .name("jaehyeon")
-                            .password("test")
-                            .email("test@gmail.com")
-                            .createdDate(LocalDateTime.now())
-                            .build()
-            );
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository,
+									BoardRepository boardRepository) throws Exception{
+		return (args)->{
+			User user = userRepository.save(
+					User.builder()
+							.name("jaehyeon")
+							.password("test")
+							.email("test@gmail.com")
+							.createdDate(LocalDateTime.now())
+							.build()
+			);
 
-            IntStream.rangeClosed(1, 200).forEach(index->
-                    boardRepository.save(Board.builder()
-                            .title("게시글" + index)
-                            .subTitle("순서" + index)
-                            .content("콘텐츠")
-                            .boardType(BoardType.free)
-                            .createdDate(LocalDateTime.now())
-                            .updatedDate(LocalDateTime.now())
-                            .user(user)
-                            .build())
-            );
-        };
-    }
+			IntStream.rangeClosed(1, 200).forEach(index->
+					boardRepository.save(Board.builder()
+							.title("게시글" + index)
+							.subTitle("순서" + index)
+							.content("콘텐츠")
+							.boardType(BoardType.free)
+							.createdDate(LocalDateTime.now())
+							.updatedDate(LocalDateTime.now())
+							.user(user)
+							.build())
+			);
+		};
+	}
 }
